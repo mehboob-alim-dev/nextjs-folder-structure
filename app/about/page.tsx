1af9ede047de3aa329ddf34b602f59024d440ac3
mehboob-alim <mehboob.alim@codistan.org>
2023-09-23 21:11:50 +0500

    header and footer added

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,14 @@
 import Card from "../(components)/card";
+import Footer from "../(components)/footer/Footer";
+import Header from "../(components)/header/Header";
 
 export default function About() {
   return (
     <>
+      <Header />
       <h1>About Page</h1>
       <Card value="About" />
+      <Footer />
     </>
   );
 }
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,10 +1,14 @@
 import Card from "../(components)/card";
+import Footer from "../(components)/footer/Footer";
+import Header from "../(components)/header/Header";
 
 export default function Services() {
   return (
     <>
+      <Header />
       <h1>Service Page</h1>
       <Card value="Service" />
+      <Footer />
     </>
   );
 }
